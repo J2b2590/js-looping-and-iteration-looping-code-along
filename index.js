be74b272776880254255397048f120d19e8034aa
J2b2590 <J2b2590@gmail.com>
2020-10-26 11:12:05 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+
+
+function writeCards(e, x){
+    let array = []
+    for (let i = 0; i < e.length; i++){
+        
+        array.push(`Thank you, ${e[i]}, for the wonderful ${x} gift!`)
+    }
+    return array;
+}
+
+
+
+function countDown(e){
+    while (e > 0){
+        console.log(e)
+        e -= 1;
+    }
+    console.log(e) 
+}
